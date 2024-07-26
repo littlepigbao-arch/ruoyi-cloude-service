f4e95161e38591ee381999dcc37fba8fc41ac03f
hsdllcw <hsdllcw@qq.com>
2024-07-26 10:42:40 +0800

    [feat] 变更基础服务地址走向nacos.zkjiadi.cc

--- ruoyi-gateway/src/main/kotlin/com/ruoyi/gateway/config/CorsFilter.kt
+++ ruoyi-gateway/src/main/kotlin/com/ruoyi/gateway/config/CorsFilter.kt
@@ -4,6 +4,8 @@ import org.apache.commons.lang3.ObjectUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.web.server.WebFilter
 
 
@@ -22,10 +24,16 @@ open class CorsConfig {
             } else {
                 response.headers["Access-Control-Allow-Origin"] = "*"
             }
+            response.headers["Access-Control-Expose-Headers"] = "*"
             response.headers["Access-Control-Allow-Credentials"] = "true"
             response.headers["Access-Control-Max-Age"] = "3600"
             response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS,HEAD"
-            response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type, Authorization, credential, X-XSRF-TOKEN, token, Admin-Token, App-Token"
+            response.headers["Access-Control-Allow-Headers"] =
+                "X-Requested-With, Content-Type, Authorization, credential, X-XSRF-TOKEN, token, Admin-Token, App-Token"
+            if (HttpMethod.OPTIONS.equals(request.method)) {
+                response.statusCode = HttpStatus.OK
+                chain.filter(exchange)
+            }
             chain.filter(exchange.mutate().request(request.mutate().build()).build())
         }
     }
